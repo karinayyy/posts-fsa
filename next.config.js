27adc672043d14a71389cb5a54980fbaa372d08e
karpenko-karina <karinakarpenko.k@gmail.com>
2023-03-08 19:45:50 +0200

    auth with google

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  image: {
+    domains: ['lh3.googleusercontent.com'],
+    formats: ['image/avif', 'image/webp'],
+  },
 }
 
 module.exports = nextConfig
